5f84546c40493cd90de335a68c24d7ab42a1b096
Peter Munene <peternjega7@gmail.com>
2021-06-25 00:46:04 +0300

    Last thing -> solns

--- js/index.js
+++ js/index.js
@@ -17,3 +17,36 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 4000); // Change image every 2 seconds
 }
+/*******
+ * 
+ * Javascript Timer just for fun
+ */
+
+        // Set the date we're counting down to
+        var countDownDate = new Date("Jun 30, 2021 00:00:25").getTime();
+
+        // Update the count down every 1 second
+        var x = setInterval(function () {
+
+            // Get today's date and time
+            var now = new Date().getTime();
+
+            // Find the distance between now and the count down date
+            var distance = countDownDate - now;
+
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            // Output the result in an element with id="demo"
+            document.getElementById("demo").innerHTML = '<span class="text-center" >Gwala<span class="text-yellow-200">Digital</span></span>' + '<span class="text-right"> ' + days + " days " + hours + "hrs "
+                + minutes + "min " + seconds + "s " + "</span> ";
+
+            // If the count down is over, write some text 
+            if (distance < 0) {
+                clearInterval(x);
+                document.getElementById("demo").innerHTML = "EXPIRED";
+            }
+        }, 1000);
